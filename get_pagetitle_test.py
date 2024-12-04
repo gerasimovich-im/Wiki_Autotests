24fefad5d8b3704c6029a3ec3a1560908abfdf84
Ilya Gerasimovich <gerasimovich.dev@gmail.com>
2024-12-04 08:59:39 +0300

    add pagetitle check

--- get_pagetitle_test.py
+++ get_pagetitle_test.py
@@ -4,5 +4,8 @@ with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
     page.goto('https://en.wikipedia.org')
-    print(page.title())
+    page.wait_for_load_state('load')
+    title = page.title()
+    assert 'Wikipedia' in title, f"Expected 'Wikipedia' in title, but got {title}"
+    print(f"{title} - test passed")
     browser.close()
